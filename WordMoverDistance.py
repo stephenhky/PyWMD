e8fdefb2fe18a348c8fc17e4a38748a60f725cd5
stephenhky <stephenhky@yahoo.com.hk>
2017-07-31 00:33:46 -0400

    modularizing further

--- WordMoverDistance.py
+++ WordMoverDistance.py
@@ -2,11 +2,14 @@ import numpy as np
 from scipy.spatial.distance import euclidean
 from scipy.sparse import dok_matrix
 from gensim.corpora import Dictionary
+from gensim.models import keyedvectors
+import cvxopt
 
 singleindexing = lambda m, i, j: m*i+j
 unpackindexing = lambda m, k: (k/m, k % m)
 
-def word_mover_distance(first_sent_tokens, second_sent_tokens):
+
+def index_tokens(first_sent_tokens, second_sent_tokens):
     tokendict = Dictionary([first_sent_tokens, second_sent_tokens])
     numwords = len(tokendict.token2id)
 
@@ -16,3 +19,12 @@ def word_mover_distance(first_sent_tokens, second_sent_tokens):
     d2vec = dok_matrix((1, numwords))
     for idx, cnt in tokendict.doc2bow(second_sent_tokens):
         d2vec[0, idx] = cnt
+
+    d1vec /= np.sum(d1vec)
+    d2vec /= np.sum(d2vec)
+
+    return tokendict, d1vec, d2vec
+
+
+def word_mover_distance(first_sent_tokens, second_sent_tokens, wvmodel):
+    _, d1vec, d2vec = index_tokens(first_sent_tokens, second_sent_tokens)
