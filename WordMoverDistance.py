b4aa83e55f38d26f7698a2b13de6cbac09230ee4
stephenhky <stephenhky@yahoo.com.hk>
2017-08-06 17:20:04 -0400

    LP provided in CVXopt requires a rank. not desirable. should switch to LuLP

--- WordMoverDistance.py
+++ WordMoverDistance.py
@@ -1,5 +1,4 @@
 
-from itertools import product
 
 import numpy as np
 from scipy.spatial.distance import euclidean
@@ -40,6 +39,7 @@ def word_mover_distance(first_sent_tokens, second_sent_tokens, wvmodel):
             c[singleindexing(numwords, i, j)] = distance
             c[singleindexing(numwords, j, i)] = distance
 
+    # normal constraint
     G = dok_matrix((len(first_sent_tokens)+len(second_sent_tokens), numwords*numwords))
     h = np.zeros(len(first_sent_tokens)+len(second_sent_tokens))
     for i in range(len(first_sent_tokens)):
@@ -55,20 +55,41 @@ def word_mover_distance(first_sent_tokens, second_sent_tokens, wvmodel):
             G[len(first_sent_tokens)+j, singleindexing(numwords, token1idx, token2idx)] = 1
             h[len(first_sent_tokens)+j] = d2vec[0, token2idx]
 
-    print d1vec.toarray()[0, :]
-    print d2vec.toarray()[0, :]
+    # additional constraints
+    emptyi = np.where(d1vec.toarray()[0, :]==0)[0]
+    emptyj = np.where(d2vec.toarray()[0, :] == 0)[0]
+    A = dok_matrix((len(emptyi)*len(second_sent_tokens)+len(emptyj)*len(first_sent_tokens), numwords*numwords))
+    b = np.zeros(len(emptyi)*len(second_sent_tokens)+len(emptyj)*len(first_sent_tokens))
+    Arowidx = 0
+    for i in emptyi:
+        for j in range(len(second_sent_tokens)):
+            A[Arowidx, singleindexing(numwords, i, j)] = 1
+            b[Arowidx] = 0
+            Arowidx += 1
+    for j in emptyj:
+        for i in range(len(first_sent_tokens)):
+            A[Arowidx, singleindexing(numwords, i, j)] = 1
+            b[Arowidx] = 0
+            Arowidx += 1
+
+    # print d1vec.toarray()[0, :]
+    # print d2vec.toarray()[0, :]
 
     print c.shape
     print G.toarray().shape
     print h.shape
+    print A.toarray().shape
+    print b.shape
 
-    print tokendict.token2id
+    # print tokendict.token2id
+    #
+    # print matrix(c)
+    # print G.toarray()
+    # print matrix(h)
+    # print A.toarray()
+    # print matrix(b)
 
-    print matrix(c)
-    print G.toarray()
-    print matrix(h)
-    # TODO: use linear object optimization
-    sol = solvers.lp(matrix(c), matrix(G.toarray()), matrix(h), solver='glpk')
+    sol = solvers.lp(matrix(c), matrix(G.toarray()), matrix(h), matrix(A.toarray()), matrix(b))
 
     return sol
 
