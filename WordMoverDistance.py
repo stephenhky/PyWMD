16854931ddbf40207fe9b93875a7fa4b0d28b3a7
stephenhky <stephenhky@yahoo.com.hk>
2017-08-12 23:18:52 -0400

    optimization algorithm implemented; need works to extract values out

--- WordMoverDistance.py
+++ WordMoverDistance.py
@@ -1,5 +1,7 @@
 
 from itertools import product
+from collections import defaultdict
+import tempfile
 
 import numpy as np
 from scipy.spatial.distance import euclidean
@@ -29,6 +31,13 @@ def index_tokens(first_sent_tokens, second_sent_tokens):
 
     return tokendict, d1vec, d2vec
 
+
+def tokens_to_fracdict(tokens):
+    cntdict = defaultdict(lambda : 0)
+    for token in tokens:
+        cntdict[token] += 1
+    totalcnt = sum(cntdict.values())
+    return {token: float(cnt)/totalcnt for token, cnt in cntdict.items()}
 
 # too difficult to maintain
 def word_mover_distance_cvxopt(first_sent_tokens, second_sent_tokens, wvmodel):
@@ -98,14 +107,29 @@ def word_mover_distance_cvxopt(first_sent_tokens, second_sent_tokens, wvmodel):
 
 
 # use PuLP
-def word_mover_distance(first_sent_tokens, second_sent_tokens, wvmodel):
+def word_mover_distance(first_sent_tokens, second_sent_tokens, wvmodel, lpFile=None):
     all_tokens = list(set(first_sent_tokens+second_sent_tokens))
     wordvecs = {token: wvmodel[token] for token in all_tokens}
+
+    first_sent_buckets = tokens_to_fracdict(first_sent_tokens)
+    second_sent_buckets = tokens_to_fracdict(second_sent_tokens)
 
     T = pulp.LpVariable.dicts('T_matrix', list(product(all_tokens, all_tokens)), lowBound=0)
 
     prob = pulp.LpProblem('WMD', sense=pulp.LpMinimize)
+    prob += pulp.lpSum([T[token1, token2]*euclidean(wordvecs[token1], wordvecs[token2])
+                        for token1, token2 in product(all_tokens, all_tokens)])
+    for token2 in second_sent_buckets:
+        prob += pulp.lpSum([T[token1, token2] for token1 in first_sent_buckets])==second_sent_buckets[token2]
+    for token1 in first_sent_buckets:
+        prob += pulp.lpSum([T[token1, token2] for token2 in second_sent_buckets])==first_sent_buckets[token1]
 
+    if lpFile!=None:
+        prob.writeLP(lpFile)
+
+    prob.solve()
+
+    return prob
 
 # example: tokens1 = ['american', 'president']
 #          tokens2 = ['chinese', 'chairman', 'king']
